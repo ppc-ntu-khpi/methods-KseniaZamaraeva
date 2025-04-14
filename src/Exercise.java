783a9df0e97bc622a80089a82ca10388e96abf24
Ksenia <159811892+KseniaZamaraeva@users.noreply.github.com>
2025-04-14 11:26:20 +0300

    Add files via upload
    Update

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,27 @@
-//package domain;
+// package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    public static int Calculate(int day, int month, int year) {
+        int sum = sumDigits(day) + sumDigits(month) + sumDigits(year);
+        return reduceToSingleDigit(sum);
+    }
+
+    // Метод для підрахунку суми цифр числа
+    private static int sumDigits(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    // Метод для зведення до однозначного числа
+    private static int reduceToSingleDigit(int number) {
+        while (number > 9) {
+            number = sumDigits(number);
+        }
+        return number;
     }
 }
